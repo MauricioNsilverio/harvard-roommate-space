7974308b9016b4383f4f88eb959d8f40d6ea36a9
mauricionsilverio <mauricionsilverio@gmail.com>
2019-12-10 19:57:24 -0300

    Updates

---
+++ precache-manifest.7f9d3d8b9636fd11d99f3e7584460db8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db613db00a77d3c8571768ce71880dcf",
+    "url": "/harvard-roommate-space/index.html"
+  },
+  {
+    "revision": "bde7a40476a0fb28c81b",
+    "url": "/harvard-roommate-space/static/css/main.3ba0b934.chunk.css"
+  },
+  {
+    "revision": "2f104cb5d0678d25b8e7",
+    "url": "/harvard-roommate-space/static/js/2.861963ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/harvard-roommate-space/static/js/2.861963ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bde7a40476a0fb28c81b",
+    "url": "/harvard-roommate-space/static/js/main.df4cccdf.chunk.js"
+  },
+  {
+    "revision": "fca116835d099ce6a025",
+    "url": "/harvard-roommate-space/static/js/runtime-main.54d62de2.js"
+  },
+  {
+    "revision": "4b1631a761edc0ab2d14ec62e014456f",
+    "url": "/harvard-roommate-space/static/media/avatar.4b1631a7.JPG"
+  },
+  {
+    "revision": "35702e26e20e3a36cd921047d4a33431",
+    "url": "/harvard-roommate-space/static/media/harvard.35702e26.png"
+  }
+]);
